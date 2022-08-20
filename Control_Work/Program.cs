30d4759a8d0fae30f636c97d7a7024250a6ee106
SkupovArt <skripachmaster39@gmail.com>
2022-08-20 21:07:28 +0400

    Создал метод, печатающий массив

--- Control_Work/Program.cs
+++ Control_Work/Program.cs
@@ -13,3 +13,12 @@ string[] Array(int lengthArray)
     }
     return array;
 }
+void PrintArr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + " ");
+        else Console.Write(array[i] + "]");
+    }
+}
